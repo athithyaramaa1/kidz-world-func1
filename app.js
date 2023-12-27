a20e229a5b1da92d702c55f525c75eac7ef4be8f
athithyaramaa1 <athithya.ramma@kalvium.community>
2023-12-27 18:19:16 +0530

    Kids World Functional Part 1

---
+++ app.js
@@ -0,0 +1,111 @@
+let items = [
+  {
+    name: "This was our pact",
+    quantity: 0,
+    dollars: 7,
+    cents: 49,
+  },
+  {
+    name: "The famous five",
+    quantity: 0,
+    dollars: 4,
+    cents: 59,
+  },
+  {
+    name: "Matilda",
+    quantity: 0,
+    dollars: 6,
+    cents: 80,
+  },
+  {
+    name: "Harry Potter",
+    quantity: 0,
+    dollars: 10,
+    cents: 0,
+  },
+  {
+    name: "For Young Readers",
+    quantity: 0,
+    dollars: 7,
+    cents: 29,
+  },
+  {
+    name: "Wimpy Kid - DIY",
+    quantity: 0,
+    dollars: 4,
+    cents: 99,
+  },
+  {
+    name: "Dart Board",
+    quantity: 0,
+    dollars: 17,
+    cents: 49,
+  },
+  {
+    name: "Connect Four",
+    quantity: 0,
+    dollars: 19,
+    cents: 99,
+  },
+  {
+    name: "Jenga",
+    quantity: 0,
+    dollars: 20,
+    cents: 99,
+  },
+  {
+    name: "Monopoly",
+    quantity: 0,
+    dollars: 19,
+    cents: 49,
+  },
+  {
+    name: "Bookmarks",
+    quantity: 0,
+    dollars: 12,
+    cents: 49,
+  },
+  {
+    name: "Birthday Card",
+    quantity: 0,
+    dollars: 12,
+    cents: 49,
+  },
+  {
+    name: "Stuffed toys",
+    quantity: 0,
+    dollars: 15,
+    cents: 99,
+  },
+  {
+    name: "Dream catcher drawing",
+    quantity: 0,
+    dollars: 18,
+    cents: 49,
+  },
+];
+
+let allButtons = document.querySelectorAll(".button");
+let cartValue = document.querySelector("#cart-value");
+let cartButton = document.querySelector("#cart");
+
+let count = 0;
+
+for (let i = 0; i < items.length; i++) {
+  allButtons[i].addEventListener("click", (e) => {
+    count++;
+
+    cartValue.innerHTML = count;
+
+    items[i].quantity++;
+  });
+}
+
+cartButton.addEventListener("click", (e) => {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].quantity > 0) {
+      let printInConsole = `Item name: ${items[i].name} -- Quantity: ${items[i].quantity}`;
+      console.log(printInConsole);
+    }
+  }
+});
